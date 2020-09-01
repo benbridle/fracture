ddbc10692c825f2f0a384a6c8a4c6e0d0fcbf77d
Ben Bridle <benjamin@pikpok.com>
2020-09-02 09:57:19 +1200

    Make DecoratedWindow render a border

--- src/components/widget.rs
+++ src/components/widget.rs
@@ -1,6 +1,15 @@
 use super::Point;
 use super::Screen;
 
+struct BorderChars {
+    horizontal: char,
+    vertical: char,
+    top_left: char,
+    top_right: char,
+    bot_left: char,
+    bot_right: char,
+}
+
 pub trait Widget {
     fn get_preferred_width(&self) -> usize;
     fn get_preferred_height(&self) -> usize;
@@ -23,6 +32,28 @@ impl Widget for DecoratedWindow {
         5
     }
     fn render(&self, screen: &mut Screen) {
-        screen.draw_char(Point::zero(), 'T');
+        let right = screen.width - 1;
+        let bottom = screen.height - 1;
+        let border = BorderChars {
+            horizontal: '─',
+            vertical: '│',
+            top_left: '┌',
+            top_right: '┐',
+            bot_left: '└',
+            bot_right: '┘',
+        };
+
+        for x in 0..screen.width {
+            screen.draw_char(Point::new(x, 0), border.horizontal);
+            screen.draw_char(Point::new(x, bottom), border.horizontal);
+        }
+        for y in 0..screen.height {
+            screen.draw_char(Point::new(0, y), border.vertical);
+            screen.draw_char(Point::new(right, y), border.vertical);
+        }
+        screen.draw_char(Point::new(0, 0), border.top_left);
+        screen.draw_char(Point::new(right, 0), border.top_right);
+        screen.draw_char(Point::new(0, bottom), border.bot_left);
+        screen.draw_char(Point::new(right, bottom), border.bot_right);
     }
 }
