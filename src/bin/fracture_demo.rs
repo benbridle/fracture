d8c87b3ad9c41f5fa71a0be4d4d814c6853d86f6
Ben Bridle <benjamin@pikpok.com>
2020-09-02 10:55:06 +1200

    Implement base Fracture object

--- src/bin/fracture_demo.rs
+++ src/bin/fracture_demo.rs
@@ -2,13 +2,7 @@ use fracture;
 use fracture::components::Widget;
 
 fn main() {
-    println!("Fracture \x1b[1mTUI\x1b[0m library");
-    println!("{:?}", fracture::components::Point::new(1, 2));
-    let mut s = fracture::components::Screen::new(10, 5);
-    let d_window = fracture::components::DecoratedWindow::new(Some(String::from("AAA")));
-    d_window.render(&mut s);
-
-    println!("{:?}", s);
-    // let frac = fracture::Fracture::new();
-    // frac.render();
+    let d_window = fracture::components::DecoratedWindow::new(Some(String::from("Root widget")));
+    let fracture = fracture::Fracture::new(d_window);
+    fracture.render_to_viewport();
 }
--- src/fracture.rs
+++ src/fracture.rs
@@ -1,1 +1,23 @@
 pub mod components;
+use components::*;
+use termion;
+
+pub struct Fracture {
+    root_widget: DecoratedWindow,
+}
+
+impl Fracture {
+    pub fn new(root_widget: DecoratedWindow) -> Fracture {
+        Fracture { root_widget }
+    }
+    pub fn render_to_viewport(&self) {
+        let size = termion::terminal_size().unwrap();
+        let width = (size.0 - 20).into();
+        let height = (size.1 - 5).into();
+
+        let mut viewport_screen = Screen::new(width, height);
+        self.root_widget.render(&mut viewport_screen);
+
+        println!("{:?}", viewport_screen);
+    }
+}
