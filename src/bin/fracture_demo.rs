5ea68c2f5838fe71b1084616298f0531aabf53ed
Ben Bridle <benjamin@pikpok.com>
2020-09-02 15:58:14 +1200

    Use DynamicLayoutWindow in fracture_demo

--- src/bin/fracture_demo.rs
+++ src/bin/fracture_demo.rs
@@ -1,12 +1,20 @@
 use fracture;
-use fracture::widgets;
+use fracture::components::DynamicRect;
+use fracture::widgets::*;
 
 fn main() {
-    let test_widget = widgets::TestWidget::new();
-    let mut sub_window = widgets::DecoratedWindow::new(Some(String::from("Sub window")));
-    let mut d_window = widgets::DecoratedWindow::new(Some(String::from("Root widget")));
+    let mut dynamic_layout = DynamicLayoutWindow::new();
+    dynamic_layout.add_sub_widget(DynamicRect::new(0, 0, 10, 5), Box::new(TestWidget::new()));
+    dynamic_layout.add_sub_widget(
+        DynamicRect::new(-20, -5, -10, -2),
+        Box::new(TestWidget::new()),
+    );
+    let mut sub_window = DecoratedWindow::new(Some(String::from("Sub window")));
+    let mut d_window = DecoratedWindow::new(Some(String::from("Root widget")));
+    let test_widget = TestWidget::new();
     sub_window.set_sub_widget(Box::new(test_widget));
-    d_window.set_sub_widget(Box::new(sub_window));
+    dynamic_layout.add_sub_widget(DynamicRect::new(15, 3, -15, -3), Box::new(sub_window));
+    d_window.set_sub_widget(Box::new(dynamic_layout));
     let fracture = fracture::Fracture::new(d_window);
     fracture.render_to_viewport();
 }
