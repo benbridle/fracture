2ca6dfc4a668ddf88aeb9eada81b26200ada28c9
Ben Bridle <benjamin@pikpok.com>
2020-09-02 16:03:10 +1200

    Add get_preferred_width/height to Widget trait

--- src/components/widget.rs
+++ src/components/widget.rs
@@ -2,4 +2,10 @@ use super::Screen;
 
 pub trait Widget {
     fn render(&self, screen: &mut Screen);
+    fn get_preferred_width(&self) -> Option<usize> {
+        return None;
+    }
+    fn get_preferred_height(&self) -> Option<usize> {
+        return None;
+    }
 }
