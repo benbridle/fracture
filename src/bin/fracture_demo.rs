001f69d26682ba92e0c5302f5c4ccfe62683d196
Ben Bridle <benjamin@pikpok.com>
2020-09-02 15:42:33 +1200

    Add TestWidget to fracture_demo

--- src/bin/fracture_demo.rs
+++ src/bin/fracture_demo.rs
@@ -2,10 +2,11 @@ use fracture;
 use fracture::widgets;
 
 fn main() {
-    let sub_widget = widgets::TestWidget::new();
-    let sub_window = widgets::DecoratedWindow::new(Some(String::from("Sub window")));
+    let test_widget = widgets::TestWidget::new();
+    let mut sub_window = widgets::DecoratedWindow::new(Some(String::from("Sub window")));
     let mut d_window = widgets::DecoratedWindow::new(Some(String::from("Root widget")));
-    d_window.set_sub_widget(Box::new(sub_widget));
+    sub_window.set_sub_widget(Box::new(test_widget));
+    d_window.set_sub_widget(Box::new(sub_window));
     let fracture = fracture::Fracture::new(d_window);
     fracture.render_to_viewport();
 }
